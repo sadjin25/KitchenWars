fd87a51eddfab47e09b8561c96a7d1964918fa51
sadjin25 <qkrdnwls0101@naver.com>
2023-06-05 19:42:57 +0900

    refac: versatile selected interactor visual code

--- Assets/Scripts/SelectedInteractorVisual.cs
+++ Assets/Scripts/SelectedInteractorVisual.cs
@@ -5,18 +5,19 @@ using System;
 
 public class SelectedInteractorVisual : MonoBehaviour
 {
-    [SerializeField] private Kitchen selectedObj;
+    [SerializeField] private IInteractor selectedObj;
     [SerializeField] private GameObject selectedVisualObj;
 
     void Start()
     {
         // Why not in Awake? -> Player Instance is may not initiallized.
         Player.Instance.OnSelectedInteractorChanged += OnSelectedKitchenChanged;
+        selectedObj = this.GetComponentInParent<IInteractor>();
     }
 
     void OnSelectedKitchenChanged(object sender, Player.OnSelectedInteractorChangedEventArgs e)
     {
-        if (e.selectedInteractor_ == (IInteractor)selectedObj)
+        if (e.selectedInteractor_ == selectedObj)
         {
             Show();
         }
