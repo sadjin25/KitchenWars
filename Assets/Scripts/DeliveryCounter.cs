62deae04e2f9389a32f768ff6dab3fde5837061f
sadjin25 <sadjin25@users.noreply.github.com>
2023-08-13 19:12:43 +0900

    feat: delivery Counter(it just remove plate)

---
+++ Assets/Scripts/DeliveryCounter.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class DeliveryCounter : KitchenObjHolder, IInteractor
+{
+    public event EventHandler OnInteract;
+
+    void Awake()
+    {
+        KitchenObjHolderInit();
+    }
+
+    public void Interact(KitchenObject objOnHand)
+    {
+        OnInteract?.Invoke(this, EventArgs.Empty);
+        if (objOnHand != null)
+        {
+            if (objOnHand.TryGetPlate(out PlateObject plateObject))
+            {
+                Player.Instance.ClearKitchenObj();
+            }
+        }
+    }
+}
